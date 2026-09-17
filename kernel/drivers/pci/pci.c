751c13546ecfaf1a47f06285fd18d1cc8d1b3432
randomusert <eetupajamaki@gmail.com>
2026-09-17 08:48:41 +0300

    PCI: prep pci device scan function

--- kernel/drivers/pci/pci.c
+++ kernel/drivers/pci/pci.c
@@ -1,42 +1,11 @@
 #include "pci.h"
+#include "pci_classes.h"
+#include <lib/std/stdio.h>
 
-static void pci_scan(
-    uint8_t bus,
-    uint8_t device,
-    uint8_t function
-) {
-    uint16_t vendor = pci_config_read16(bus, device, function, 0x00);
 
-    if (vendor == 0xFFFF)
-        return;
+/*
+ * Scan PCI Devices and register them to the driver and by extension to the whole os to use
+ */
+void scan_pci_devices() {
 
-    uint16_t device_id =
-        pci_config_read16(bus, device, function, 0x02);
-
-    uint8_t revision =
-        pci_config_read8(bus, device, function, 0x08);
-
-    uint8_t prog_if =
-        pci_config_read8(bus, device, function, 0x09);
-
-    uint8_t subclass =
-        pci_config_read8(bus, device, function, 0x0A);
-
-    uint8_t class_code =
-        pci_config_read8(bus, device, function, 0x0B);
-
-    uint8_t header =
-        pci_config_read8(bus, device, function, 0x0E);
-
-    /*
-     * At this point you have discovered a PCI device.
-     *
-     * Example:
-     *
-     * vendor      = 0x8086
-     * device_id   = ...
-     * class_code  = 0x01
-     * subclass    = 0x06
-     */
 }
-
